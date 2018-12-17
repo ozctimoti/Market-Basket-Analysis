624f8eeda6368794a29985d51a25209d0bd1b42d
Timoteos Ozcelik <T-Mac@Timoteos-MacBook-Pro.local>
2018-12-17 18:22:25 +0300

    Command lines were deleted

--- processing.py
+++ processing.py
@@ -8,13 +8,6 @@ os.chdir('./Data/')
 train = pd.read_csv('./train_middle.csv', sep='\t')
 # test = pd.read_csv('./test_middle.csv', sep='\t')
 
-'''
-train[(train.user_id == u) & (train.product_id == p)])
-print(train.query('user_id == u & product_id == p').order_number)
-order_number = train[train[train.user_id == u].product_id == p].order_number.unique() # NOT WORKED
-order_number = train.query('user_id == u & product_id = p') # NOT WORKED
-'''
-
 train['days_since_first_order'] = pd.Series((np.zeros(len(train.user_id))))
 train.days_since_first_order = train.days_since_first_order.astype(int)
 
@@ -31,19 +24,6 @@ for u in user_id:
         for o in order_number[1:]:
             train.at[(train.user_id == u) & (train.product_id == p) & (train.order_number == o), 'days_since_first_order'] \
                 = sum(days_since_prior_order[0: o])
-
-'''    
-c = 0
-for o in order_number[::-1]:
-    if c == 0:
-        train[(train.user_id == u) & (train.product_id == p) & (train.order_number == o)].next_order_day = 0
-        next_o = o
-        c += 1
-    else:
-        train.at[(train.user_id == u) & (train.product_id == p) & (train.order_number == o), 'next_order_day'] = sum(days_since_prior_order[o: next_o])
-        train[(train.user_id == u) & (train.product_id == p) & (train.order_number == next_o)].days_since_prior_order = sum(days_since_prior_order[o: next_o])
-        next_o = o
-'''
 
 train.to_csv('./train.csv', sep='\t', encoding='utf-8', index=False)
 print(train)
