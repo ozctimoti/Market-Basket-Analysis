dd545f498297cb7d08d7abacca212788ea691ee7
TimoteosOzcelik <timoteos.ozcelik@gmail.com>
2018-12-20 10:44:21 +0300

    Efficient appriori problem was fixed

--- eff_apriori.py
+++ eff_apriori.py
@@ -7,22 +7,17 @@ os.chdir('./Data')
 train = pd.read_csv('./train.csv', sep='\t')
 
 user_id = train.user_id.unique()
-for u in user_id:
-    product_id = train[train.user_id == u].product_id.unique().tolist()
-    total_order = len(train[train.user_id == u].order_number.unique())
-    ass_rul_eff = []
+for u in user_id[5:]:
     order_number = train[(train.user_id == u)].order_number.unique()
+    ass_rul_eff = []
     for o in order_number:
-        temp = []
-        for p in product_id:
-            if not (train[(train.user_id == u) & (train.order_number == o) & (train.product_id == p)]).empty:
-                temp.append(p)
-        temp = tuple(temp)
+        product_id = train[(train.user_id == u) & (train.order_number == o)].product_id.tolist()
+        temp = tuple(product_id)
         ass_rul_eff.append(temp)
-    itemsets, rules = apriori(ass_rul_eff, min_support=0.4,  min_confidence=1)
-    rules_rhs = filter(lambda rule: len(rule.lhs) == 2 and len(rule.rhs) == 1, rules)
+    print(ass_rul_eff)
+    itemsets, rules = apriori(ass_rul_eff, min_support=0.5,  min_confidence=1)
+    rules_rhs = filter(lambda rule: len(rule.rhs) == 1, rules)
     for rule in sorted(rules_rhs, key=lambda rule: rule.lift):
             print(f'User id: {u} with rule {rule}')
 
-# TODO: Association Rules Mining for user u.
 
