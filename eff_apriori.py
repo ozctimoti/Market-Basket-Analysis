e229f5237376e0bd91010141bd6398c76b513d71
TimoteosOzcelik <timoteos.ozcelik@gmail.com>
2018-12-20 16:55:19 +0300

    Problem fixed

--- eff_apriori.py
+++ eff_apriori.py
@@ -7,17 +7,17 @@ os.chdir('./Data')
 train = pd.read_csv('./train.csv', sep='\t')
 
 user_id = train.user_id.unique()
-for u in user_id[5:]:
+for u in user_id:
     order_number = train[(train.user_id == u)].order_number.unique()
     ass_rul_eff = []
     for o in order_number:
         product_id = train[(train.user_id == u) & (train.order_number == o)].product_id.tolist()
         temp = tuple(product_id)
         ass_rul_eff.append(temp)
-    print(ass_rul_eff)
-    itemsets, rules = apriori(ass_rul_eff, min_support=0.5,  min_confidence=1)
-    rules_rhs = filter(lambda rule: len(rule.rhs) == 1, rules)
+    itemsets, rules = apriori(ass_rul_eff, min_support=0.6,  min_confidence=1)
+    rules_rhs = filter(lambda rule: len(rule.lhs) == 1 and len(rule.rhs) == 1, rules)
     for rule in sorted(rules_rhs, key=lambda rule: rule.lift):
+        if len(order_number) > 4:
             print(f'User id: {u} with rule {rule}')
 
 
---
+++ test_analysis.py
@@ -0,0 +1,16 @@
+import os
+from collections import Counter
+import pandas as pd
+import numpy as np
+
+# Path configuration
+os.chdir('./Data/')
+
+test = pd.read_csv('./test.csv', sep='\t')
+order_number_test = []
+for u in test.user_id.unique():
+    order_number_test.append(len(test[test.user_id == u].order_number.unique().tolist()))
+    print(test[test.user_id == u].order_number.unique().tolist())
+    print(len(test[test.user_id == u].order_number.unique().tolist()))
+print(order_number_test, type(order_number_test))
+print(Counter(order_number_test))
