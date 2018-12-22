e88c51a7c2c9a1998bccca5df340ae43571305a9
TimoteosOzcelik <timoteos.ozcelik@gmail.com>
2018-12-22 14:57:08 +0300

    Update

--- efficient_apriori_.py
+++ efficient_apriori_.py
@@ -16,7 +16,7 @@ for u in user_id:
         product_id = train[(train.user_id == u) & (train.order_number == o)].product_id.tolist()
         temp = tuple(product_id)
         ass_rul_eff.append(temp)
-    itemsets, rules = apriori(ass_rul_eff, min_support=0.5,  min_confidence=1)
+    itemsets, rules = apriori(ass_rul_eff, min_support=0.45,  min_confidence=0.75)
     rules_rhs = filter(lambda rule: len(rule.lhs) == 1 and len(rule.rhs) == 1, rules)
     for rule in sorted(rules_rhs, key=lambda rule: rule.lift):
         if len(order_number) >= 4:
